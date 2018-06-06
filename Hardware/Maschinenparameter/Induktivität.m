8501dc8881b69accd0c4cfc0db885d5dbfc36541
CanICodeDis <313kt0ne@gmail.com>
2018-06-06 22:20:34 +0200

    ________________ < Matlab-Skripte >  ----------------       \                    / \  //\        \    |\___/|      /   \//  \\             /0  0  \__  /    //  | \ \            /     /  \/_/    //   |  \  \            @_^_@'/   \/_   //    |   \   \            //_^_/     \/_ //     |    \    \         ( //) |        \///      |     \     \       ( / /) _|_ /   )  //       |      \     _\     ( // /) '/,_ _ _/  ( ; -.    |    _ _\.-~        .-~~~^-.   (( / / )) ,-{        _      `-.|.-~-.           .~         `.  (( // / ))  '/\      /                 ~-. _ .-~      .-~^-.  \  (( /// ))      `.   {            }                   /      \  \   (( / ))     .----~-.\        \-'                 .~         \  `. \^-.              ///.----..>        \             _ -~             `.  ^-`  ^-_                ///-._ _ _ _ _ _ _}^ - - - - ~                     ~-- ,.-~                                                                   /.-~

--- Hardware/Maschinenparameter/Induktivität.m
+++ Hardware/Maschinenparameter/Induktivität.m
@@ -1,5 +1,7 @@
 1;
 
+p=7;
+
 twoRs=39e-3;
 
 t_el=[3.880e-3,5.8e-3,2.95e-3];
@@ -8,22 +10,26 @@ V_pp=[9.0e-2,0.076,0.1];
 I_pp=[1.74,1.74,1.76];
 
 freq=1./t_el
+freq_mech = freq/p
 omega=2*pi.*freq
 
-V_amp=V_pp./2;
-V_eff=V_amp./sqrt(2);
-I_amp=I_pp./2;
-I_eff=I_amp./sqrt(2);
+V_amp = V_pp/2;
+V_eff = V_amp/sqrt(2);
+I_amp=I_pp/2;
+I_eff=I_amp/sqrt(2);
 
-Z = V_eff./I_eff;
-twoL =abs( ( Z - twoRs) ./ (omega * i));
-disp("2*L aus Impedanz: "),disp(twoL);
 
 phi_deg=(dPhase./t_el)*360
 phi_rad=(phi_deg./180) * pi
-%tan(phi)=(omega*L)/R -> L = (tan(phi) * R)/ omega
+%tan(phi)=(omega*L)/R -> L = (tan(phi) * R)/omega
 twoL =( tan(phi_rad) * twoRs ) ./ omega;
 disp ("Phasenverschiebung :") , disp(phi_deg),disp(phi_rad);
-disp ("2*L: ") , disp (twoL);
+disp ("L: ") , disp (twoL);
 
-
+Vs = V_eff .* e.^(i*0);
+Is = I_eff .* e.^(i*phi_rad);
+Z = Vs./Is;
+% Z = R + jwL
+wL = imag(Z);
+L= wL ./ omega
+%tau_el = twoL/ twoRs
--- Hardware/Maschinenparameter/Ripple.m
+++
@@ -1,5 +0,0 @@
-1;
-
-%Q=CU -> I= C* dUdt -> C = I/dUdt
-I = 20
-dUdt=40
